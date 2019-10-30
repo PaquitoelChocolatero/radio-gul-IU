3f8c3576d5d3a115d29eaa9cee3db01551f40a53
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-10-31 00:48:04 +0100

    Twitter

--- radio-gul/src/App.vue
+++ radio-gul/src/App.vue
@@ -1,33 +1,50 @@
 <template>
   <div id="app">
     <Header></Header>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Twitter></Twitter>
+    <twitter id='twitter'>
+      <div slot="loading">loading .....</div>
+      <a class="twitter-timeline" href="https://twitter.com/radiogul?ref_src=twsrc%5Etfw">Tweets by Radio GUL</a>
+    </twitter>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
-import Twitter from './components/Twitter.vue'
+import Vue from 'vue'
+import twitter from 'vue-twitter'
+
+Vue.use(twitter)
 
 export default {
   name: 'app',
   components: {
     //HelloWorld
-    Header,
-    Twitter
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  /* If on mobile */
+  @media (max-width: 768px) {
+    #twitter{
+      float: right;
+      width: 55%; /*aiuda*/
+      height: 100%;
+    }
+  }
+  /* If on desktop */
+  @media (min-width: 768px) {
+    #twitter{
+      float: right;
+      width: 20%;
+      height: 100%;
+    }
+  }
 </style>
