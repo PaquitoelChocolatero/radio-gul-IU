4293e2d03a4e1e1157708cc7ecb8c0eb4c580482
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-11-07 15:11:51 +0100

    Search in progress

--- radio-gul/src/components/Header.vue
+++ radio-gul/src/components/Header.vue
@@ -2,7 +2,13 @@
     <header>
         <img src="../assets/radio.jpg" style="position: relative; width: 100%; height: 150px; border-radius: 7px;"/>
         <div class='buttons'>
-            <button id='search'>Buscar</button>
+
+            <div id="wrap">
+                <form action="" autocomplete="on">
+                    <input id="search" name="search" type="text" placeholder="Programas"><input id="search_submit" value="Rechercher" type="submit">
+                </form>
+            </div>
+
             <button id='direct'>Directo</button>
         </div>
         <div class="logo">
@@ -37,6 +43,66 @@ export default {
         right: 1%;
     }
 
-    .buttons button{
+    #wrap {
+        display: inline-block;
+        position: relative;
+        height: 60px;
+        padding: 0;
+        position: relative;
     }
+
+    input[type="text"] {
+        height: 30px;
+        font-size: 30px;
+        display: inline-block;
+        font-family: "Lato";
+        font-weight: 100;
+        border: none;
+        outline: none;
+        color: black;
+        /* padding: 3px; */
+        padding-right: 60px;
+        width: 0px;
+        position: absolute;
+        top: 0;
+        right: 0;
+        background: none;
+        z-index: 3;
+        transition: width .4s cubic-bezier(0.000, 0.795, 0.000, 1.000);
+        cursor: pointer;
+    }
+
+    input[type="text"]:focus:hover {
+        border-bottom: 1px solid rgb(51, 196, 233);
+    }
+
+    input[type="text"]:focus {
+        width: 300px;
+        z-index: 1;
+        border-bottom: 2px solid black;
+        cursor: text;
+    }
+
+    input[type="submit"] {
+        height: 67px;
+        width: 63px;
+        display: inline-block;
+        color:red;
+        background: url("../assets/search.png") center center no-repeat;
+        text-indent: -10000px;
+        border: none;
+        position: absolute;
+        top: 0;
+        right: 0;
+        z-index: 2;
+        cursor: pointer;
+        opacity: 0.4;
+        cursor: pointer;
+        transition: opacity .4s ease;
+    }
+
+    input[type="submit"]:hover {
+        opacity: 0.8;
+    }
+
 </style>
