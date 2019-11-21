6e4b1df5d938497746ac173ee281e87ca53a707b
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-11-21 12:54:25 +0100

    Footer iniciado

--- radio-gul/src/App.vue
+++ radio-gul/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    <Header></Header>
+    <Header />
     <twitter id='twitter'>
       <div slot="loading">loading .....</div>
-      <a class="twitter-timeline" href="https://twitter.com/radiogul?ref_src=twsrc%5Etfw">Tweets by Radio GUL</a>
+      <a class="twitter-timeline" data-chrome="nofooter noheader noscrollbar" data-tweet-limit="5" href="https://twitter.com/radiogul?ref_src=twsrc%5Etfw">Tweets by Radio GUL</a>
     </twitter>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 import Vue from 'vue'
 import twitter from 'vue-twitter'
 
@@ -18,8 +20,8 @@ Vue.use(twitter)
 export default {
   name: 'app',
   components: {
-    //HelloWorld
-    Header
+    Header,
+    Footer
   }
 }
 </script>
@@ -34,17 +36,19 @@ export default {
   /* If on mobile */
   @media (max-width: 768px) {
     #twitter{
+      margin-top: 1%;
+      margin-right: 1%;
       float: right;
-      width: 55%; /*aiuda*/
-      height: 100%;
+      width: 33%;
     }
   }
   /* If on desktop */
   @media (min-width: 768px) {
     #twitter{
+      margin-top: 1%;
+      margin-right: 1%;
       float: right;
       width: 20%;
-      height: 100%;
     }
   }
 </style>
---
+++ radio-gul/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <h1>"Test"</h1>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+    h1{
+        color: white;
+    }
+    footer {
+        margin-bottom: 0;
+        width: 100%;
+        background-color: black;
+    }
+</style>
