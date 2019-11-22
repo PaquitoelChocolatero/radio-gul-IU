489a7c9629d7c831d13a0c1170b54adad15374b3
Juan Francisco Garcia <juanfgarciacas@gmail.com>
2019-11-22 17:31:57 +0100

    Added a bg image to header

--- radio-gul/src/components/Header.vue
+++ radio-gul/src/components/Header.vue
@@ -1,29 +1,24 @@
 <template>
-    <header>
-        <img src="../assets/radio.jpg" style="position: relative; width: 100%; height: 150px; border-radius: 7px;"/>
-        <div class="logo">
-            <img src="../assets/radiogul.jpg" style="width: 100px; height: 100px; border: solid 2px blue;"/>
-        </div>
-    </header>
+    <div class="header" :style="{ backgroundImage: `url(${bg})` }">
+        
+    </div>
 </template>
 
 <script>
+import bg from '../assets/back.jpg'
 export default {
-    name: 'Header'
+    name: 'Header',
+    data() {
+        return {
+            bg
+        }
+  }
 }
 </script>
 
 <style scoped>
-    header {
-        margin-top: 1%;
-        margin-left: 1%;
-        width: 98%;
-    }
+    .header{
+        height: 400px;
 
-    .logo {
-        position: absolute;
-        z-index: 100;
-        left: 5%;
-        top: 100px;
     }
 </style>
