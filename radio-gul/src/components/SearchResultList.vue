33081c2796403cf48917d098102b1525f25c5436
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-11-25 12:40:03 +0100

    SearchResult page

---
+++ radio-gul/src/components/SearchResultList.vue
@@ -0,0 +1,84 @@
+<template>
+    <div id='list'>
+        <p id='title'>Resuls for test</p>
+        <div id='grid'>
+            <SearchResult v-bind:key='program.id' v-for='program in programs' v-bind:program="program" />
+        </div>
+    </div>
+</template>
+
+<script>
+import SearchResult from "./SearchResult.vue";
+
+export default {
+    name: 'SearchResultList',
+    components: {
+        SearchResult
+    },
+    data(){
+        return{
+            programs: [
+                {
+                    id: 1,
+                    name: '/dev/random',
+                    img: require('../assets/devrandom.jpg')
+                },
+                {
+                    id: 2,
+                    name: 'MeetUC3M',
+                    img: require('../assets/meetuc3m.jpg')
+                },
+                {
+                    id: 3,
+                    name: 'test1',
+                    img: require('../assets/meetuc3m.jpg')
+                },
+                {
+                    id: 4,
+                    name: 'test2',
+                    img: require('../assets/meetuc3m.jpg')
+                },
+                {
+                    id: 5,
+                    name: 'test3',
+                    img: require('../assets/meetuc3m.jpg')
+                },
+                {
+                    id: 6,
+                    name: 'test4',
+                    img: require('../assets/meetuc3m.jpg')
+                },
+                {
+                    id: 7,
+                    name: 'test5',
+                    img: require('../assets/meetuc3m.jpg')
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #list{
+        float: left;
+        width: 76%;
+        height: 100%;
+        margin-top: 50px;
+        margin-left: 1%;
+    }
+    #grid{
+        width: 100%;
+        height: 100%;
+        display: list;
+        grid-template-columns: auto auto auto;
+    }
+    #title{
+        text-align: left;
+        margin-left: 1%;
+        font-size: 1.2em;
+        border-bottom: 1px solid black;
+        width: 100%;
+        color: black;
+    }
+</style>
