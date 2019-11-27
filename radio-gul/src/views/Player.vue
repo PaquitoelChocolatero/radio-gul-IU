73f182a0f7523b741d52e01245103aa91d22971e
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-11-27 17:12:37 +0100

    Nested routes

---
+++ radio-gul/src/views/Player.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="player">
+    <AudioPlayer />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from '../components/Footer.vue'
+import AudioPlayer from '../components/AudioPlayer.vue'
+
+export default {
+  name: 'player',
+  components: {
+    Footer,
+    AudioPlayer
+  }
+}
+</script>
+
+<style>
+  html{
+    height: 100%;
+  }
+  #player {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #f2f2f3;
+  }
+</style>
