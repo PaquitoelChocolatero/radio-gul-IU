c863db38b84abe7ff5adaf94b7150f189d5db494
juanbanpar <100383373@alumnos.uc3m.es>
2019-11-27 19:03:13 +0100

    Starting program page

--- radio-gul/src/components/Episodes.vue
+++ radio-gul/src/components/Episodes.vue
@@ -1,11 +1,32 @@
 <template>
-  <div v-on:click='jump()'>
-      <h1>Test</h1>
+  <div temporadas>
+      <h1>Temporadas</h1>
+      <div v-on:click='jump()'>
+          <h2>Test</h2>
+      </div>
+
+      <badger-accordion>
+        <badger-accordion-item>
+          <template slot="header">First Accordion Header</template>
+          <template slot="content">First Accordion Content</template>
+        </badger-accordion-item>
+
+        <badger-accordion-item>
+          <template slot="header">Second Accordion Header</template>
+          <template slot="content">Second Accordion Content</template>
+        </badger-accordion-item>
+      </badger-accordion>
   </div>
 </template>
 
 <script>
+import {BadgerAccordion, BadgerAccordionItem} from 'vue-badger-accordion'
+
 export default {
+  components: {
+        BadgerAccordion,
+        BadgerAccordionItem
+  },
   name: 'episodes',
   methods: {
     jump(){
