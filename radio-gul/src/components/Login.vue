62df3a8b5a12d621babd538b350f759bd2ee8491
commonhater <gba007@hotmail.es>
2019-11-30 18:40:11 +0100

    first steps in the login

--- radio-gul/src/components/Login.vue
+++ radio-gul/src/components/Login.vue
@@ -1,13 +1,20 @@
 <template>
-  <div id="login">
-<<<<<<< HEAD
-    <input type='checkbox' id='form-switch'>
-        <form id='login-form' method='post'>
-          <input type="text" placeholder="Username" required>
-          <input type="password" placeholder="Password" required>
-          <button type='submit'>Login</button>
-          <label for='form-switch'><span>Register</span></label>
-        </form>
+    <div id="login">
+        <div class="login-boxes">
+          <input type='checkbox' id='form-switch'>
+              <form id='login-form' method='post'>
+              
+                <input type="text" class="user" placeholder="Usuario" required>
+                <input type="password" placeholder="Contraseña" required>
+                <br>
+                <button type='login-button'>Login</button>
+                  <p>
+                  <br>
+                    New administrator?</p>  
+                <button v-on:click="jump()">Register</button>
+
+              </form>
+        </div>
         <!-- <form id='register-form' method='post'>
           <input type="text" placeholder="Nombre de usuario" required>
           <input type="email" placeholder="Email" required>
@@ -25,19 +32,77 @@
           <button type='submit'>Register</button>
           <label for='form-switch'>¿Ya tiene una cuenta? Inicie sesión</label>
         </form> -->
-=======
-    
->>>>>>> 1196c93c515d0428adbdb9809bba94b2fef252f2
-  </div>
+    </div>
 </template>
 
 <script>
 
 export default {
-  name: 'login'
+  name: 'login',
+
+  methods: {
+    jump(){
+        this.$router.push({
+            name: 'register'
+        })
+    }
+  }
+  
 }
 </script>
 
-<style scoped>
+<style>
+
+.login-boxes{
+  /*border-radius: 2px;*/
+    margin: 15% auto;
+
+  border-color:black;
+  width: 25%;
+  
+}
+
+.user{
+  margin-top: 10px;
+}
+
+  form {
+    width:100%;
+    border: 2px solid #CCC;
+
+  }
+  input {
+    margin-top : 3px;
+    margin-bottom:3px;
+    padding:10px;
+    width: 80%;
+    border:1px solid #CCC;
+  }
+  button {
+    padding:5px;
+    border-radius: 2px;
+    width: 70%;
+  }
+  label {
+    cursor:pointer;
+    color: white;
+  }
+  #form-switch {
+    display:none;
+  }
+  #register-form {
+    display:none;
+  }
+  #form-switch:checked~#register-form {
+    display:block;
+  }
+  #form-switch:checked~#login-form {
+    display:none;
+  }
+
+   #logo {
+    float: left;
+    margin-left: 10pt;
+  }
 
 </style>
---
+++ radio-gul/src/components/Register.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="login">
+    <input type='checkbox' id='form-switch'>
+        <form id='login-form' method='post'>
+          <input type="text" placeholder="Username" required>
+          <input type="password" placeholder="Password" required>
+          <button type='register-button'>Register</button>
+            <p>Already have an account?</p>
+          <button src="../assets/admin.png" alt="admin" width="40" height="40" v-on:click="jump()">Login</button>
+
+
+          <label for='form-switch'><span>Register</span></label>
+        </form>
+        <!-- <form id='register-form' method='post'>
+          <input type="text" placeholder="Nombre de usuario" required>
+          <input type="email" placeholder="Email" required>
+          <input type="password" placeholder="Contraseña" required>
+          <input type="text" placeholder="Nombre y apellidos" required>
+          <input type="date" required>
+          <select name="Intereses">
+            <option value="None selected">Seleccione abajo</option>
+            <option value="PC">PC</option>
+            <option value="Play Station">Play Station</option>
+            <option value="Nintendo">Nintendo</option>
+          </select> 
+          <input type="text" placeholder="Uso de la aplicacion">
+          <input type="checkbox" required><label><span>Condiciones de Uso y Privacidad</span></label>
+          <button type='submit'>Register</button>
+          <label for='form-switch'>¿Ya tiene una cuenta? Inicie sesión</label>
+        </form> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'register',
+
+  methods: {
+    jump(){
+        this.$router.push({
+            name: 'login'
+        })
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+
+</style>
