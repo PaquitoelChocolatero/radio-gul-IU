2cdc589373303778408026877095d91b44d865c9
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-12-02 12:38:55 +0100

    More comment styling

--- radio-gul/src/components/Comments.vue
+++ radio-gul/src/components/Comments.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="comments">
-      Este podcast es una mierda
+    <p id='title'>Comments</p>
+    <div id='input'>
+      <input type="text" placeholder="Type a comment...">
+    </div>
+    <div id='comment'>
+      <p id='user'> </p>
+      <p id='text'> Este podcast es una mierda </p>
+    </div>
   </div>
 </template>
 
@@ -12,17 +19,40 @@ export default {
 </script>
 
 <style scoped>
-    div{
-        float: left;
-        cursor: default;
-        text-align: left;
-    }
-    #comments{
-        margin-top: 3%;
-        padding: 1% 1% 1% 1%;
-        margin-bottom: 3%;
-        background-color: white;
-        border-radius: 5px;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    }
+  div{
+    cursor: default;
+    text-align: left;
+  }
+  input{
+    margin-top: 1%;
+    margin-left: 1%;
+    padding: 1% 1% 1% 1%;
+  }
+  #title{
+    font-weight: bold;
+    color: rgb(0, 0, 0, 0.7);
+    text-align: left;
+    margin-left: 0.1%;
+    padding-left: 1%;
+    font-size: 1.2em;
+    border-bottom: 1px solid black;
+    width: 100%;
+    color: black;
+  }
+  #comments{
+    width: 100%;
+    margin-top: 3%;
+    padding: 1% 1% 1% 1%;
+    margin-bottom: 3%;
+    background-color: white;
+    border-radius: 5px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+  #comment{
+    margin: 2% 2% 1% 1%; /* UP - RIGHT - DOWN - LEFT */
+    padding: 0.2% 1% 0.2% 1%;
+    border-radius: 5px;
+    width: 98%;
+    background-color: #8ac3de;
+  }
 </style>
