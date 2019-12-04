92f0c70c0df08371ef1a114238c2fa20454496fc
juanbanpar <100383373@alumnos.uc3m.es>
2019-12-04 11:40:30 +0100

    Better accordion margins

--- radio-gul/src/components/Episodes.vue
+++ radio-gul/src/components/Episodes.vue
@@ -64,7 +64,7 @@ export default {
   background-color: white;
   width: 47%;
   margin-left: 51%;
-  margin-bottom: 5%;
+  margin-bottom: 1%;
   border-radius: 5px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
