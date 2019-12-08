4a9ec246d2a54898e4440921141ae062780e9538
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-12-08 02:22:34 +0100

    Admin almost done

--- radio-gul/src/views/Admin.vue
+++ radio-gul/src/views/Admin.vue
@@ -12,6 +12,11 @@
 
     <div v-if="access == 1" id="admin">
       <HeaderAdmin />
+      <jsoneditor v-model="json" :options="options" :plus="false" id='editor' @error="onError" />
+      <div id='uploads'>
+        <upload v-model="fileList" :url="remoteUrl" name="Images" id='uploadImage'/>
+        <upload v-model="fileList" :url="remoteUrl" name="Audio" />
+      </div>
     </div>
 
   </div>
@@ -20,19 +25,29 @@
 <script>
 import Login from "../components/Login.vue";
 import HeaderAdmin from "../components/HeaderAdmin.vue";
-import ClickOutside from 'vue-click-outside'
+import ClickOutside from 'vue-click-outside';
+import Vue from 'vue';
+import jsoneditor from 'v-jsoneditor/src/index';
+import json from "@/jsons/data.json";
+import upload from "vux-uploader-component";
+
+Vue.use(jsoneditor)
 
 export default {
   name: 'admin',
   data(){
     return{
       access: 0,
-      flag: 0
+      flag: 0,
+      json: json,
+      fileList: []
     }
   },
   components: {
     Login,
-    HeaderAdmin
+    HeaderAdmin,
+    jsoneditor,
+    upload
   },
   methods: {
     grantAccess(){
@@ -44,6 +59,9 @@ export default {
     },
     hideError(){
       this.flag = false
+    },
+    onError() {
+      this.console.log('error')
     }
   },
   directives: {
@@ -79,5 +97,19 @@ export default {
   .fill{
     height: 1px;
     background-color: #f2f2f3;
+  }
+  #editor{
+    height: 540px;
+    width: 80%;
+    margin-left: 10%;
+    margin-top: 50px;
+  }
+  #uploads{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #uploadImage{
+    margin-right: 100px;
   }
 </style>
